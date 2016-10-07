453aecd777440438a50d55d66eb9340c1c45f043
Rohan <rohan.peshkar284@gmail.com>
2016-10-07 11:40:51 +0530

    Added second activity

--- app/src/main/java/me/rohanpeshkar/allinone/MainActivity.java
+++ app/src/main/java/me/rohanpeshkar/allinone/MainActivity.java
@@ -1,7 +1,11 @@
 package me.rohanpeshkar.allinone;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btn_click)
+    void onClickBtn() {
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
     }
 }
